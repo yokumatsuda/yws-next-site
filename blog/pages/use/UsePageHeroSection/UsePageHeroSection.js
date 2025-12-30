5d44106551ec891da859b4e1356d18ed09b0330a
Yoku Matsuda <yoku3103@gmail.com>
2025-12-31 06:48:27 +0900

    updata by matsuda

---
+++ blog/pages/use/UsePageHeroSection/UsePageHeroSection.js
@@ -0,0 +1,35 @@
+import styles from "./UsePageHeroSection.module.css";
+
+function UsePageHeroSection() {
+  return (
+    <section className={styles.heroSection}>
+      {/* 画像（右側 / モバイル時はトップ） */}
+      <div className={styles.heroImage}>
+        <img
+          src="https://ywd-digital-contents.s3.ap-northeast-1.amazonaws.com/yws-wp/services/HeroSection_img3.jpeg"
+          alt="About Us"
+        />
+      </div>
+
+      {/* テキスト（左側） */}
+      <div className={styles.heroContent}>
+        <h1 className={styles.pageTitle}>ご利用の流れ</h1>
+        <p className={styles.subtitle}>簡単ステップで、すぐにサービスをご利用いただけます。</p>
+
+        <div className={styles.buttonGroup}>
+          <a href="/contact" className={styles.heroButton}>
+            お問い合わせ
+          </a>
+          <a href="/News" className={styles.heroButton}>
+            お知らせ
+          </a>
+          <a href="/#sectionTitle1" className={styles.heroButton}>
+            Home
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default UsePageHeroSection;
